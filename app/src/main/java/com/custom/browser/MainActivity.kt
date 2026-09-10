3c0f45d9109d81bf458f842f4c07c24d684fd2c8
nanasichero <nathan2029.sichero@gmail.com>
2026-09-10 12:21:44 -0300

    Refactor MainActivity to use WebView directly
    Refactor MainActivity to use WebView directly and simplify back navigation.

--- app/src/main/java/com/custom/browser/MainActivity.kt
+++ app/src/main/java/com/custom/browser/MainActivity.kt
@@ -1,43 +1,34 @@
 package com.nanasichero.nanafox
 
+import android.app.Activity
 import android.os.Bundle
-import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.OnBackPressedCallback
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        webView = findViewById(R.id.webview)
-        
-        val settings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.loadsImagesAutomatically = true
-        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        // Cria o WebView diretamente para evitar erros de layout ou tema
+        webView = WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            webViewClient = WebViewClient()
+            loadUrl("https://www.google.com")
+        }
 
-        webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
+        setContentView(webView)
+    }
 
-        // Carrega a página com segurança
-        webView.loadUrl("https://www.google.com")
-
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                if (webView.canGoBack()) {
-                    webView.goBack()
-                } else {
-                    finish()
-                }
-            }
-        })
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
