036ac9c23f25dc6107b93bf695c99e3ae9e8454b
nanasichero <nathan2029.sichero@gmail.com>
2026-09-10 12:17:42 -0300

    Refactor WebView settings in MainActivity
    Updated WebView settings for improved functionality and security.

--- app/src/main/java/com/custom/browser/MainActivity.kt
+++ app/src/main/java/com/custom/browser/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nanasichero.nanafox
 
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -16,16 +17,19 @@ class MainActivity : AppCompatActivity() {
 
         webView = findViewById(R.id.webview)
         
-        val webSettings: WebSettings = webView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.domStorageEnabled = true
-        webSettings.loadWithOverviewMode = true
-        webSettings.useWideViewPort = true
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadsImagesAutomatically = true
+        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
 
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        // Carrega a página com segurança
         webView.loadUrl("https://www.google.com")
 
-        // Tratamento moderno para o botão de voltar do celular
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if (webView.canGoBack()) {
